3aa29ae77d2523ae1cc1feb5161ce459c105390b
yangkechuan <907148778@qq.com>
2017-03-09 11:24:16 +0800

    文件名错误修改

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ APP_PATH=$(cd `dirname $0`; pwd)
 
 cd $APP_PATH
 
-py.test > apiTest.log  --html=report.html
+py.test > apiTest.log  --html=report.html --self-contained-html
+
 
 echo "run time: `date '+%Y-%m-%d %H:%M:%S'`" >> run.log
 
