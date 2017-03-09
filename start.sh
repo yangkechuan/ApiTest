51b854d3c9d1e662ada6aa26076215b789a1697f
yangkechuan <907148778@qq.com>
2017-03-09 11:06:10 +0800

    增加run,log，记录日志

--- start.sh
+++ start.sh
@@ -4,5 +4,13 @@ APP_PATH=$(cd `dirname $0`; pwd)
 
 cd $APP_PATH
 
-py.test > apiTest.log  --html=report.html --self-contained-html;
+py.test > apiTest.log  --html=report.html --self-contained-html
+
+echo "run time: `date '+%Y-%m-%d %H:%M:%S'` \n" >> run.log
+
+cat apitest.log >> run.log
+
+echo "\n\n" >> run.log
+
+
 python3 checkError.py;
